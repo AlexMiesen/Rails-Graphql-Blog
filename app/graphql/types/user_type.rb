0d54908357589fe32ba0a3419aa0c1950d0b5887
AlexMiesen <alex@i-motor.com.au>
2022-06-28 23:34:13 +1000

    Added more fields in user_type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,7 +1,12 @@
 class Types::UserType < Types::BaseObject
 	description "One User"
-	
+
 	field :id, ID, null: false
 	field :first_name, String, null: false
 	field :last_name, String, null: false
+	field :street, String, null: false
+	field :number, Int, null: false
+	field :city, String, null: false
+	field :postcode, Int, null: false
+	field :country, String, null: false
 end
