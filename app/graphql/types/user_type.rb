0539f35be1a32de2e69c6719f3ce0dd85599fcb6
AlexMiesen <alex@i-motor.com.au>
2022-06-30 18:23:48 +1000

    created a user seed and added full_address

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -9,16 +9,15 @@ class Types::UserType < Types::BaseObject
 	field :city, String, null: false
 	field :postcode, Int, null: false
 	field :country, String, null: false
-	# field :full_name, String, null: false
+	field :full_address, String, null: true
+
+	def full_address
+		"#{object.number} #{object.street}, #{object.postcode}, #{object.city}, #{object.country}"
+	end
 
 	field :posts, [Types::PostType], null: true
 
 	def posts
 		Post.all
-	end 
-
-	# def full_name
-	# 	`#{:number}, #{:street}, #{:postcode}, #{:city}, #{:country}`
-	# end
-
+	end
 end
